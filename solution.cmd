acc22d9c0550f7fa34b1f2add022518e50d149dd
joelvaneenwyk <joel.vaneenwyk@gmail.com>
2023-08-13 11:29:04 -0700

    Add support for VS2022 to `solution.cmd`

--- solution.cmd
+++ solution.cmd
@@ -1,5 +1,13 @@
 @echo off
-start "" "%~dp0.build\vs2019\clink.sln"
-sleep 2
-call code "%~dp0"
+setlocal EnableDelayedExpansion
+
+set "clink_solution=%~dp0.build\vs2019\clink.sln"
+if not exist "!clink_solution!" set "clink_solution=%~dp0.build\vs2022\clink.sln"
+if not exist "!clink_solution!" (
+    echo Failed to find Visual Studio solution for clink: "!clink_solution!"
+    exit /b 90
+)
+
 cd /d "%~dp0"
+echo Launching Clink solution in Visual Studio: "!clink_solution!"
+start "" "!clink_solution!"
